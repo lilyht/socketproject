06a884e50b46a061d51c027a53a3421bfe21c00e
lilyht <295808612@qq.com>
2020-03-13 20:35:22 +0800

    create floder

---
+++ pan/testdb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import MySQLdb
+
+# 打开数据库连接
+db = MySQLdb.connect("localhost", "root", "yrp990716", "pandb", charset='utf8' )
+
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+
+# 使用execute方法执行SQL语句
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取一条数据
+data = cursor.fetchone()
+
+print("Database version :")
+print(data)
+
+# 关闭数据库连接
+db.close()
